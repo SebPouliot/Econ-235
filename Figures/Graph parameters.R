c1e9523633194c7bfa8e9052b2bccb3c4d844a5a
Sebastien Pouliot <pouliot@iastate.edu>
2018-09-21 13:34:47 -0500

    Updated options section
    Updated options section. Started working on livestock section.

--- Figures/Graph parameters.R
+++ Figures/Graph parameters.R
@@ -9,19 +9,19 @@ suppressMessages(loadfonts(device = "win")) #Load the fonts
 pacman::p_load(viridis)
 
 #Setup for the figures
-mytheme <- theme(plot.title = element_text(size = 10, colour = 'black', family = "Times New Roman"),
-                  text=element_text(size=10, family = "Times New Roman", colour = 'black'),
+mytheme <- theme(plot.title = element_text(size = 12, colour = 'black', family = "Times New Roman"),
+                  text=element_text(size=12, family = "Times New Roman", colour = 'black'),
                   axis.ticks.x = element_line(color='black'), 
                   axis.ticks.y = element_line(color='black'), 
                   axis.text.y = element_text(colour = 'black', family = "Times New Roman"), 
-                  axis.text.x = element_text(size = 10, colour = 'black', family = "Times New Roman"),
+                  axis.text.x = element_text(size = 12, colour = 'black', family = "Times New Roman"),
                   axis.title = element_text(colour = 'black', family = "Times New Roman"),
                   panel.border = element_blank(), 
                   axis.line.x = element_line(color='black'),  
                   axis.line.y = element_line(color='black'),  
                   plot.margin=unit(c(0.5,0.5,0.5,0.5), "lines"), 
                   legend.title=element_blank(), 
-                  legend.text = element_text(size = 8, colour = 'black', family = "Times New Roman"), 
+                  legend.text = element_text(size = 12, colour = 'black', family = "Times New Roman"), 
                   legend.background=element_blank(),
                   legend.key=element_blank(),
                   legend.position="bottom")
